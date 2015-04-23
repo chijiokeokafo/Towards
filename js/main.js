23e8237c234b0dc69f97d84e52a20b4581f9760b
Chijioke Okafo <chijiokeokafo@gmail.com>
2015-04-22 21:33:25 -0400

    Initial commit, adding original provided files

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// Shorthand for $( document ).ready()
+$(function() {
+	console.log("ready!");
+
+	document.body.addEventListener('click', menu);
+
+	var men = 0;
+
+	$("#menu-button").click(function() {
+		console.log("clicked");
+
+		$("#main-nav").toggleClass("show");
+
+	});
+
+	function menu() {
+		men++
+		if (($("#main-nav").attr("class") == "hide show") && (men > 1)) {
+			$("#main-nav").toggleClass("show");
+			men = 0;
+			console.log(men);
+		} else {
+			console.log("no");
+		}
+	}
+
+	var waypoint = new Waypoint({
+		element: document.getElementById('lightbox'),
+		handler: function() {
+			console.log("THIS");
+			$("body").toggleClass("black");
+		}
+	});
+
+});
